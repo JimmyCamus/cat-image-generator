c69a251ec09dd51b812563c1613c0e888eddf73a
Jimmy Camus <jeremy.camus.varela@gmail.com>
2023-01-29 00:03:27 -0300

    feat: styles has been added

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,45 @@ import { useInput } from './hooks/inputs.hooks';
 
 const App = () => {
   const { value, handleOnChange } = useInput();
-  const { catUrl, handleGetCats, handleGetFactCat } = useCat();
+  const { catUrl, isLoading, handleGetCats, handleGetFactCat } = useCat();
   return (
-    <main>
-      <h1>Generate Random Cat Image With Custom Text!</h1>
-      <input value={value} onChange={handleOnChange} type="text" />
-      <button onClick={(e) => handleGetCats(value)} type="submit">
-        Kitty Time!
-      </button>
-      <span>--OR--</span>
-      <button onClick={(e) => handleGetFactCat()}>Generate random cat with a fact</button>
-      {catUrl && (
-        <img width={500} src={catUrl} alt="random cat with custom text from cataas.com api" />
-      )}
+    <main className="flex flex-col items-center">
+      <div className="w-1/2">
+        <h1 className="text-3xl my-5 text-center ">Generate Random Cat Image With Custom Text</h1>
+        <section className="flex flex-col items-center">
+          <div>
+            <input
+              className="input input-primary"
+              value={value}
+              onChange={handleOnChange}
+              placeholder="Enter your sentence"
+              type="text"
+            />
+            <button className="btn mx-4" onClick={(e) => handleGetCats(value)} type="submit">
+              Kitty Time!
+            </button>
+          </div>
+          <div className="w-full flex flex-row items-center my-5">
+            <span className={`w-full h-[1px] bg-secondary`}></span>
+            <span className="mx-3">OR</span>
+            <span className={`w-full h-[1px] bg-secondary`}></span>
+          </div>
+          <button className="btn" onClick={(e) => handleGetFactCat()}>
+            Generate random cat with a fact
+          </button>
+        </section>
+        <section className="flex flex-col items-center my-8">
+          {isLoading ? (
+            <progress className="progress progress-secondary"></progress>
+          ) : (
+            <img
+              className="max-w-[700px] max-h-[700px]"
+              src={catUrl}
+              alt="random cat with custom text from cataas.com api"
+            />
+          )}
+        </section>
+      </div>
     </main>
   );
 };
--- src/hooks/cats.hooks.ts
+++ src/hooks/cats.hooks.ts
@@ -9,14 +9,18 @@ const CAT_FACT_ENDPOINT = 'https://catfact.ninja/fact?max_length=50';
 
 export const useCat = () => {
   const [catUrl, setCatUrl] = useState<string>('');
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const handleGetCats = async (sentence: string) => {
+    setIsLoading(true);
     const res = await fetch(CREATE_CAT_ENDPOINT(sentence));
     const { url } = await res.json();
     setCatUrl(`${CAT_ENDPOINT_PREFIX}${url}`);
+    setIsLoading(false);
   };
 
   const handleGetFactCat = async () => {
+    setIsLoading(true);
     const res = await fetch(CAT_FACT_ENDPOINT);
     const { fact } = await res.json();
     handleGetCats(fact);
@@ -26,5 +30,5 @@ export const useCat = () => {
     handleGetFactCat();
   }, []);
 
-  return { catUrl, handleGetCats, handleGetFactCat };
+  return { catUrl, isLoading, handleGetCats, handleGetFactCat };
 };
